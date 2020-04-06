44bfaf63e1c798c2bc6f08cda1dd5544d2f2dd36
Joaquin <joaquinollerogarcia@gmail.com>
2020-04-06 22:26:13 +0100

    Initial commit.

---
+++ src/Application.kt
@@ -0,0 +1,28 @@
+package com.madeofair
+
+import freemarker.cache.ClassTemplateLoader
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.freemarker.FreeMarker
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Made of Air", contentType = ContentType.Text.Plain)
+        }
+    }
+}
+
