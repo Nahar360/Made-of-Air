020e8fe9d3153a99adf44afd9be1843736d6b804
Joaquin <joaquinollerogarcia@gmail.com>
2020-04-07 17:11:52 +0100

    Created home page.

---
+++ src/webapp/pages/Home.kt
@@ -0,0 +1,30 @@
+package com.madeofair.webapp.pages
+
+import com.madeofair.models.UserSession
+import com.madeofair.repositories.UsersRepository
+import io.ktor.application.call
+import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.locations.Location
+import io.ktor.locations.get
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.sessions.get
+import io.ktor.sessions.sessions
+
+const val HOME = "/"
+
+@Location(HOME)
+class Home
+
+fun Route.home(usersRepository: UsersRepository) {
+    get<Home> {
+        val user = call.sessions.get<UserSession>()?.let { usersRepository.get(it.userId) }
+
+        call.respond(
+            FreeMarkerContent(
+                "home.ftl",
+                mapOf("user" to user)
+            )
+        )
+    }
+}
