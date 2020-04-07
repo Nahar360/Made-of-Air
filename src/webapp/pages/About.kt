948d7bee516955626afe68b19801c7e4f3f1add3
Joaquin <joaquinollerogarcia@gmail.com>
2020-04-07 17:11:33 +0100

    Created about page.

---
+++ src/webapp/pages/About.kt
@@ -0,0 +1,30 @@
+package com.madeofair.webapp.pages
+
+import com.madeofair.models.UserSession
+import com.madeofair.repositories.UsersRepository
+import io.ktor.application.call
+import io.ktor.freemarker.FreeMarkerContent
+import io.ktor.locations.Location
+import io.ktor.locations.get
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.sessions.get
+import io.ktor.sessions.sessions
+
+const val ABOUT = "/about"
+
+@Location(ABOUT)
+class About
+
+fun Route.about(usersRepository: UsersRepository) {
+    get<About> {
+        val user = call.sessions.get<UserSession>()?.let { usersRepository.get(it.userId) }
+
+        call.respond(
+            FreeMarkerContent(
+                "about.ftl",
+                mapOf("user" to user)
+            )
+        )
+    }
+}
